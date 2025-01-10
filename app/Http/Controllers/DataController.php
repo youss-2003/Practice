43a40cbe187d6d1e02b107021a80fb16874b7075
youss-2003 <youssef.ratbi222@gmail.com>
2025-01-10 12:57:12 +0100

    v 01

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class DataController extends Controller
 {
-    //
+    public function store(Request $request){
+       $validate=([
+        
+       ])
+    }
+
+    public function index(){
+
+    }
 }
